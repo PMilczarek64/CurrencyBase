54126463f4a8bb69d8eb045633c8b10a2fba67c0
Mammoth64 <mammoth1907@gmail.com>
2022-08-02 22:42:59 +0200

    Add first tests - Jest and React testing library

---
+++ src/utils/__tests__/convertPLNToUSD.js
@@ -0,0 +1,26 @@
+import { convertPLNToUSD } from './../convertPLNtoUSD';
+
+describe('ConvertPLNtoUSD', () => {
+  it('should return proper value when good input', () => {
+    expect(convertPLNToUSD(1)).toBe('$0.29');
+    expect(convertPLNToUSD(2)).toBe('$0.57');
+    expect(convertPLNToUSD(20)).toBe('$5.71');
+    expect(convertPLNToUSD(12)).toBe('$3.43');
+  });
+  it('should return NaN when input is text', () => {
+    expect(convertPLNToUSD('abc')).toBeNaN();
+    expect(convertPLNToUSD('6')).toBeNaN();
+    expect(convertPLNToUSD('')).toBeNaN();
+    expect(convertPLNToUSD('@#W')).toBeNaN();
+  });
+  it('should return NaN when input is empty', () => {
+    expect(convertPLNToUSD()).toBeNaN();
+  });
+  it('should return Error when input is NaN and i not a string', () => {
+    expect(convertPLNToUSD({})).toBe('Error');
+    expect(convertPLNToUSD([])).toBe('Error');
+    expect(convertPLNToUSD(function() {})).toBe('Error');
+    expect(convertPLNToUSD(null)).toBe('Error');
+    expect(convertPLNToUSD(Boolean)).toBe('Error');
+  })
+});
